59aabab5ee752dd21fe9fa17905429dd5d6cc5a6
Parth <parthyadav141@gmail.com>
2024-11-07 00:28:44 +0530

    NODE ENV

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  // origin: process.env.NODE_ENV === "production" ? "https://your-production-domain.com" : "http://localhost:5173",
-  origin: "http://localhost:5173",
+  origin: process.env.NODE_ENV === "production" ? "https://your-frontend-production-domain.com" : "http://localhost:5173",
   credentials: true, 
   optionsSuccessStatus: 200,
 };

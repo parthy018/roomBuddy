4816bb3c6ff2d4cefe2dd57cedf1f2f2e0553492
Ravikant Choudhary <ravikantchoudhary537@gmail.com>
2024-12-23 13:01:01 +0530

    user allready existv while register, model file formating

--- server/models/roommate.model.js
+++ server/models/roommate.model.js
@@ -1,22 +1,54 @@
-const mongoose = require('mongoose');
-const { propertyHighlights,amenities } = require('../utills/Enums');
-const roommateSchema = new mongoose.Schema({
-    place: { type: String, required: true },
-    description: { type: String, required: true },
-    rent: { type: Number, required: true },
-    image: [{ type: String, required: true }],
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    lookingGender: { type: String, enum: ["male", "female", "Any"], required: true },
-    occupancy: { type: String, enum: ["Single", "Shared", "Any"], required: true },
-    highlights: [{
+const mongoose = require("mongoose");
+const { propertyHighlights, amenities } = require("../utills/Enums");
+const roommateSchema = new mongoose.Schema(
+  {
+    place: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    rent: {
+      type: Number,
+      required: true,
+    },
+    image: [
+      {
         type: String,
         required: true,
-        
-    }],
-    amenities: [{
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    lookingGender: {
+      type: String,
+      enum: ["male", "female", "Any"],
+      required: true,
+    },
+    occupancy: {
+      type: String,
+      enum: ["Single", "Shared", "Any"],
+      required: true,
+    },
+    highlights: [
+      {
         type: String,
         required: true,
-    }],
-}, { timestamps: true });
+      },
+    ],
+    amenities: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Roommate', roommateSchema);
+module.exports = mongoose.model("Roommate", roommateSchema);
